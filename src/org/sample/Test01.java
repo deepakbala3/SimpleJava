35c49b8b5e81bea0d90701c9051862e010031846
deepakbala3 <spbalasubramaniyan66@gmail.com>
2023-08-17 20:42:16 +0530

    staging area to local repo

---
+++ src/org/sample/Test01.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Number 1");
+		System.out.println("Number 2");
+		System.out.println("Number 3");
+
+	}
+
+}
